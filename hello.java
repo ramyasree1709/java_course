d8e1fc6eac144a69a96b8a4a0a49360d1792e454
Ramya Sree <ramyasree.mca2025@adhiyamaan.in>
2026-08-11 11:36:21 +0530

    Changed into arithmetic operations

--- hello.java
+++ hello.java
@@ -5,12 +5,13 @@ class hello
     public static void main(String args[])
     {
         Scanner ramya=new Scanner(System.in);
-        String name=ramya.nextLine();
-        int age=ramya.nextInt();
-        ramya.nextLine();
-        String address=ramya.nextLine();
-        System.out.println("My name is "+name);
-        System.out.println("My age is "+age);
-        System.out.println("My address is "+address);
+        int a=ramya.nextInt();
+        int b=ramya.nextInt();
+        int c=ramya.nextInt();
+        int d=a*b*c;
+        int e=a+b+c;
+        int f=d/e;
+        System.out.print("The value is "+f);
+      
     }
 }
