4c3c76426016be6b996ba0fe0c4ab1b87215fe0e
ramyasree1709 <ramyasree.mca2025@adhiyamaan.in>
2026-08-13 23:47:51 +0530

    Nested loop

---
+++ nestedfor.java
@@ -0,0 +1,25 @@
+//to print * (3 rows and 3columns) using nested for loop
+import java.util.Scanner;
+class demo
+{
+    public static void main(String[] args)
+    {
+        
+        for(int i=0;i<=2;i++)
+        {
+            
+            for(int j=0;j<=2;j++)
+            {
+                System.out.print("*");
+            }
+           System.out.println();
+        }
+    
+    
+}
+}
+      
+
+
+
+
