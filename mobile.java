aab2acb4729adc0c8d19d52a85891f22933ecec0
Ramya Sree <ramyasree.mca2025@adhiyamaan.in>
2026-08-17 14:54:43 +0530

    functions

---
+++ mobile.java
@@ -0,0 +1,28 @@
+public class mobile{
+    void vivo()
+    {
+        System.out.println("this is Vivo");
+    }
+    void realme()
+    {
+        System.out.println("this is realme");
+    }
+    void samsung()
+    {
+        System.out.println("this is samsung");
+    }
+     void oppo()
+    {
+        System.out.println("this is oppo");
+    }
+
+
+    public static void main(String args[])
+    {
+     mobile obj1=new mobile();
+     obj1.vivo();
+     obj1.realme();
+     obj1.samsung();
+     obj1.oppo();
+    }
+}
